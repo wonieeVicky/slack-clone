32aca11c1851de047cf9860312faa95ff39811df
Vicky <hwfongfing@gmail.com>
2021-10-09 23:47:32 +0900

    feat: 라우터 설정 확인을 위한 기본 로그인, 회원가입 페이지 생성 및 라우터 적용

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,16 @@
-﻿import React from 'react';
+﻿import React, { FC } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import LogIn from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
-const App = () => {
-  return <div>HELLO WORLD!</div>;
+const App: FC = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
