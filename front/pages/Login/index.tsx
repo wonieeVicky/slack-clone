c0ea55022247c750a7ceaccb6554960f2a9ea58f
Vicky <hwfongfing@gmail.com>
2021-10-13 22:57:46 +0900

    feat: 로그인페이지 컴포넌트 생성

--- front/pages/Login/index.tsx
+++ front/pages/Login/index.tsx
@@ -1,7 +1,59 @@
-﻿import React from 'react';
+﻿import React, { useState, useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import useInput from '@hooks/useInput';
+import { Form, Error, Label, Input, LinkContainer, Button, Header } from '@pages/SignUp/styles';
 
 const LogIn = () => {
-  return <div>로그인</div>;
+  const [logInError, setLogInError] = useState(false);
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      setLogInError(false);
+      axios
+        .post('/api/users/login', { email, password })
+        .then((response) => {})
+        .catch((error) => {
+          setLogInError(error.response?.data?.statusCode === 401);
+        });
+    },
+    [email, password],
+  );
+
+  // console.log(error, userData);
+  // if (!error && userData) {
+  //   console.log('로그인됨', userData);
+  //   return <Redirect to="/workspace/sleact/channel/일반" />;
+  // }
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+          {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
 };
 
 export default LogIn;
