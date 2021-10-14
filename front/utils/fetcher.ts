0bb65fcd8ee3ed3181262045157929c5c71dc60b
Vicky <hwfongfing@gmail.com>
2021-10-14 23:19:49 +0900

    feat: fetcher 유틸 함수 구현

---
+++ front/utils/fetcher.ts
@@ -0,0 +1,10 @@
+﻿import axios from 'axios';
+
+const fetcher = (url: string) =>
+  axios
+    .get(url, {
+      withCredentials: true,
+    })
+    .then((response) => response.data); // response.data가 useSWR의 data가 된다.
+
+export default fetcher;
