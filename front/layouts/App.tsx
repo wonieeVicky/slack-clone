40c245e353d69aedb38571993b26484f6a0988b3
Vicky <hwfongfing@gmail.com>
2021-10-16 23:54:09 +0900

    feat: App 컴포넌트에 /workspace/channel Route 추가

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -3,6 +3,7 @@ import loadable from '@loadable/component';
 import { Switch, Route, Redirect } from 'react-router-dom';
 const LogIn = loadable(() => import('@pages/Login'));
 const SignUp = loadable(() => import('@pages/SignUp'));
+const Channel = loadable(() => import('@pages/Channel'));
 
 const App: FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: FC = () => {
       <Redirect exact path="/" to="/login" />
       <Route path="/login" component={LogIn} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/workspace/channel" component={Channel} />
     </Switch>
   );
 };
