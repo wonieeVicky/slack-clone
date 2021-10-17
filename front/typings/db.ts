16ae9d1dd4ed5f10e737853ff391eca986e0c86e
Vicky <hwfongfing@gmail.com>
2021-10-17 22:42:28 +0900

    feat: 유저 및 워크스페이스 타이핑 추가

---
+++ front/typings/db.ts
@@ -0,0 +1,13 @@
+﻿export interface IUser {
+  id: number;
+  nickname: string;
+  email: string;
+  Workspaces: IWorkspace[];
+}
+
+export interface IWorkspace {
+  id: number;
+  name: string;
+  url: string; // 주소 창에 보이는 주소
+  OwnerId: number; // 워크스페이스 만든 사람 아이디
+}
