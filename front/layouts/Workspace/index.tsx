5b483f7308843c269a051d12a06606817c995e68
Vicky <hwfongfing@gmail.com>
2021-10-18 10:51:47 +0900

    feat: 워크스페이스 스타일 추가 및 gravatar 적용

---
+++ front/layouts/Workspace/index.tsx
@@ -0,0 +1,57 @@
+﻿import { IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React, { FC, useCallback } from 'react';
+import { Redirect } from 'react-router-dom';
+import useSWR from 'swr';
+import {
+  Header,
+  RightMenu,
+  ProfileImg,
+  WorkspaceWrapper,
+  Workspaces,
+  Channels,
+  WorkspaceName,
+  Chats,
+  MenuScroll,
+} from './styles';
+import gravatar from 'gravatar';
+
+// FC 타입 안에 children이 들어있음, children을 안쓸 경우 VFC로 사용한다.
+const Workspace: FC = ({ children }) => {
+  const { data, error, mutate } = useSWR<IUser | false>('/api/users', fetcher, {
+    dedupingInterval: 100000, // 100초
+  });
+
+  const onLogout = useCallback(
+    () => axios.post('/api/users/logout', null, { withCredentials: true }).then(() => mutate(false, false)),
+    [],
+  );
+
+  if (!data) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
+      <button onClick={onLogout}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>MenuScroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
+    </div>
+  );
+};
+
+export default Workspace;
