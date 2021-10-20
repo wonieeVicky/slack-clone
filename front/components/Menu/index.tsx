5a91ff8bee69dc0b699b221f0604df666115c142
Vicky <hwfongfing@gmail.com>
2021-10-20 17:30:39 +0900

    feat: 공용 컴포넌트 Menu 구현

---
+++ front/components/Menu/index.tsx
@@ -0,0 +1,33 @@
+﻿import { CreateMenu, CloseModalButton } from '@components/Menu/styles';
+import React, { CSSProperties, FC, useCallback } from 'react';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  style: CSSProperties;
+  closeButton?: boolean;
+}
+
+const Menu: FC<Props> = ({ closeButton, style, show, children, onCloseModal }) => {
+  // 부모 태그로 이벤트 버블링 방지
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  if (!show) {
+    return null;
+  }
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div onClick={stopPropagation} style={style}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
+};
+Menu.defaultProps = {
+  closeButton: true,
+};
+
+export default Menu;
