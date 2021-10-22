02872d24c9d3f032b0145dbfc9ffc1398e0f0510
Vicky <hwfongfing@gmail.com>
2021-10-22 23:57:43 +0900

    feat: Modal 공용 컴포넌트 구현

---
+++ front/components/Modal/index.tsx
@@ -0,0 +1,29 @@
+﻿import { CloseModalButton } from '@components/Menu/styles';
+import React, { FC, useCallback } from 'react';
+import { CreateModal } from './styles';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+}
+
+const Modal: FC<Props> = ({ show, children, onCloseModal }) => {
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <CreateModal onClick={onCloseModal}>
+      <div onClick={stopPropagation}>
+        <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>
+        {children}
+      </div>
+    </CreateModal>
+  );
+};
+
+export default Modal;
