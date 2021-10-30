81fb3a1c8121928e64dd01feccfa60a9b437b52d
Vicky <hwfongfing@gmail.com>
2021-10-30 21:57:01 +0900

    feat: ChannelList 컴포넌트 구현

---
+++ front/components/ChannelList/index.tsx
@@ -0,0 +1,51 @@
+﻿// import useSocket from '@hooks/useSocket';
+import { CollapseButton } from '@components/DMList/styles';
+import { IChannel, IChat, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import useSWR from 'swr';
+
+const ChannelList = () => {
+  const { workspace } = useParams<{ workspace?: string }>();
+  // const [socket] = useSocket(workspace);
+  const { data: userData, mutate: revalidateUser } = useSWR<IUser>('/api/users', fetcher, {
+    dedupingInterval: 2000,
+  });
+  const { data: channelData } = useSWR<IChannel[]>(userData ? `/api/workspaces/${workspace}/channels` : null, fetcher);
+  const [channelCollapse, setChannelCollapse] = useState(false);
+
+  const toggleChannelCollapse = useCallback(() => setChannelCollapse((prev) => !prev), []);
+
+  return (
+    <>
+      <h2>
+        <CollapseButton collapse={channelCollapse} onClick={toggleChannelCollapse}>
+          <i
+            className="c-icon p-channel_sidebar__section_heading_expand p-channel_sidebar__section_heading_expand--show_more_feature c-icon--caret-right c-icon--inherit c-icon--inline"
+            data-qa="channel-section-collapse"
+            aria-hidden="true"
+          />
+        </CollapseButton>
+        <span>Channels</span>
+      </h2>
+      <div>
+        {!channelCollapse &&
+          channelData?.map((channel) => {
+            return (
+              <NavLink
+                key={channel.name}
+                activeClassName="selected"
+                to={`/workspace/${workspace}/channel/${channel.name}`}
+              >
+                <span># {channel.name}</span>
+              </NavLink>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+export default ChannelList;
