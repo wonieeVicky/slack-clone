f0a4f84c11713e602e68dbb6781d230a64167a0f
Vicky <hwfongfing@gmail.com>
2021-10-31 23:46:03 +0900

    feat: DM 컴포넌트에 user별 아이콘 및 ChatList, ChatBox 렌더링 구현

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,7 +1,32 @@
 ﻿import React from 'react';
+import { Container, Header } from './styles';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import { IUser } from '@typings/db';
+import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
 
 const DirectMessage = () => {
-  return <div>Direct Message :)</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR<IUser>(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData?.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData?.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat="" />
+    </Container>
+  );
 };
 
 export default DirectMessage;
