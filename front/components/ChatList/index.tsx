820064bb3445756cbc619faf3bfef3fb11a7a4b9
Vicky <hwfongfing@gmail.com>
2021-10-31 23:45:36 +0900

    feat: DM 내 ChatList 컴포넌트 베이스 구현

---
+++ front/components/ChatList/index.tsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import { ChatZone, Section } from './styles';
+
+const ChatList = () => {
+  return (
+    <ChatZone>
+      <Section></Section>
+    </ChatZone>
+  );
+};
+
+export default ChatList;
