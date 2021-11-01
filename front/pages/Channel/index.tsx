0c38441b0757c336fe2dd802c8dd26b971320518
Vicky <hwfongfing@gmail.com>
2021-11-01 23:58:40 +0900

    feat: Channel, DM 내 ChatBox props 이벤트 생성 및 반영

--- front/pages/Channel/index.tsx
+++ front/pages/Channel/index.tsx
@@ -1,10 +1,22 @@
-﻿import React from 'react';
+﻿import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
 
 const Channel = () => {
+  const [chat, onChangeChat, setChat] = useInput('');
+  const onSubmitForm = useCallback((e) => {
+    // DM 보내기
+    e.preventDefault();
+    console.log('submit');
+    setChat('');
+  }, []);
   return (
     <Container>
       <Header>'채널'</Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+﻿import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
 import gravatar from 'gravatar';
 import useSWR from 'swr';
@@ -7,11 +7,19 @@ import { IUser } from '@typings/db';
 import { useParams } from 'react-router';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR<IUser>(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
+
+  const [chat, onChangeChat] = useInput('');
+  const onSubmitForm = useCallback((e) => {
+    // DM 보내기
+    e.preventDefault();
+    console.log('submit');
+  }, []);
 
   if (!userData || !myData) {
     return null;
@@ -24,7 +32,7 @@ const DirectMessage = () => {
         <span>{userData?.nickname}</span>
       </Header>
       <ChatList />
-      <ChatBox chat="" />
+      <ChatBox chat="" onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };
