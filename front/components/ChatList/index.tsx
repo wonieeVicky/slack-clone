4ba280fb8bbdaa145c5e3772b03b4705f190246f
Vicky <hwfongfing@gmail.com>
2021-11-08 23:51:17 +0900

    feat: ChatList에 react-custom-scrollbars 적용

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,18 +1,23 @@
-﻿import React, { VFC } from 'react';
-import { ChatZone, Section } from './styles';
+﻿import React, { useCallback, useRef, VFC } from 'react';
+import { ChatZone } from './styles';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
   return (
     <ChatZone>
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
