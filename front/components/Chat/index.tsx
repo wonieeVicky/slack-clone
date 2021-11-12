f99557d6b46cfd4106bccc355adf92d633ba8508
Vicky <hwfongfing@gmail.com>
2021-11-13 00:08:32 +0900

    feat: Chat 컴포넌트 성능 최적화(memo, useMemo)

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,5 +1,5 @@
 ﻿import { IDM } from '@typings/db';
-import React, { VFC } from 'react';
+import React, { VFC, memo, useMemo } from 'react';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
@@ -16,21 +16,29 @@ const Chat: VFC<Props> = ({ data }) => {
   const user = data.Sender;
   // \d 숫자, +는 1개 이상 ?는 0개나 1개, *는 0개 이상을 의미함
   // g는 모두 찾기, |는 또는, \n은 줄바꿈
-  const result = regexifyString({
-    input: data.content,
-    pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
-    decorator(match, index) {
-      const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
-      if (arr) {
-        return (
-          <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
-            @{arr[1]}
-          </Link>
-        );
-      }
-      return <br key={index} />;
-    },
-  });
+  // a?.b; // optional chaining
+  // a??b; // nullish coalescing
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+          console.log(arr);
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />;
+        },
+      }),
+    [data.content],
+  );
+
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -41,10 +49,10 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
 };
 
-export default Chat;
+export default memo(Chat);
