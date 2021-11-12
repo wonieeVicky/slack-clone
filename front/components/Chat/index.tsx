9a0f40c8713fbb3878d4384b75836c4c7c76e810
Vicky <hwfongfing@gmail.com>
2021-11-12 23:59:08 +0900

    feat: Chat에 정규표현식으로 문자열 변환 처리

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -3,13 +3,34 @@ import React, { VFC } from 'react';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
 
 interface Props {
   data: IDM;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
   const user = data.Sender;
+  // \d 숫자, +는 1개 이상 ?는 0개나 1개, *는 0개 이상을 의미함
+  // g는 모두 찾기, |는 또는, \n은 줄바꿈
+  const result = regexifyString({
+    input: data.content,
+    pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+    decorator(match, index) {
+      const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+      if (arr) {
+        return (
+          <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+            @{arr[1]}
+          </Link>
+        );
+      }
+      return <br key={index} />;
+    },
+  });
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -20,7 +41,7 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{}</p>
       </div>
     </ChatWrapper>
   );
