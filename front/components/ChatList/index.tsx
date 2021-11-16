b1c38a8d7e2f9133ea9c2ee843008642980dc341
Vicky <hwfongfing@gmail.com>
2021-11-16 11:39:41 +0900

    feat: DirectMessage, ChatList에 makeSection 컴포넌트 적용

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,22 +1,31 @@
 ﻿import React, { useCallback, useRef, VFC } from 'react';
-import { ChatZone } from './styles';
+import { ChatZone, Section, StickyHeader } from './styles';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
-  chatData?: IDM[];
+  chatSections: { [key: string]: IDM[] };
 }
 
-const ChatList: VFC<Props> = ({ chatData }) => {
+const ChatList: VFC<Props> = ({ chatSections }) => {
   const scrollbarRef = useRef(null);
   const onScroll = useCallback(() => {}, []);
   return (
     <ChatZone>
       <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
-        {chatData?.map((chat) => (
-          <Chat key={chat.id} data={chat} />
-        ))}
+        {Object.entries(chatSections).map(([date, chats]) => {
+          return (
+            <Section className={`section-${date}`} key={date}>
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+              {chats.map((chat) => (
+                <Chat key={chat.id} data={chat} />
+              ))}
+            </Section>
+          );
+        })}
       </Scrollbars>
     </ChatZone>
   );
