beadf97761a23d15831266ab3226c85593542aa2
Vicky <hwfongfing@gmail.com>
2021-11-30 16:07:13 +0900

    feat: 개발환경에서 swrdevtools 적용

--- front/client.tsx
+++ front/client.tsx
@@ -3,10 +3,17 @@ import { render } from 'react-dom';
 
 import App from '@layouts/App';
 import { BrowserRouter } from 'react-router-dom';
+import SWRDevtools from '@jjordy/swr-devtools';
 
 render(
   <BrowserRouter>
-    <App />
+    {process.env.NODE_ENV === 'production' ? (
+      <App />
+    ) : (
+      <SWRDevtools>
+        <App />
+      </SWRDevtools>
+    )}
   </BrowserRouter>,
   document.querySelector('#app'),
 );
